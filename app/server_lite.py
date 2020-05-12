06a5faac83865308f18aa362eb0d4fa3efa2bbf9
Seth <sethtomy@gmail.com>
2020-05-12 09:54:04 -0400

    Switching to TensorFlow lite

--- app/server_lite.py
+++ app/server_lite.py
@@ -11,7 +11,7 @@ output_details = interpreter.get_output_details()
 
 # Test model on random input data.
 input_shape = input_details[0]['shape']
-input_data = np.array(np.random.random_sample(input_shape), dtype=np.float32)
+input_data = np.array(np.random.random_sample(input_shape), dtype=np.uint8)
 interpreter.set_tensor(input_details[0]['index'], input_data)
 
 interpreter.invoke()
