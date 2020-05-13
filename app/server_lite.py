a7fb8eeb1ba15d41ea6ba76713203323acfc923a
Seth <sethtomy@gmail.com>
2020-05-13 00:31:55 -0400

    API server integration

--- app/server_lite.py
+++ app/server_lite.py
@@ -1,6 +1,8 @@
 import numpy as np
 import distro
 from PIL import Image
+import flask
+import io
 
 DISTRO = distro.id()
 print('Current platform is: ' + DISTRO)
@@ -12,39 +14,63 @@ else:
 MODEL_FILE = '../data/inception_v4_299_quant.tflite'
 LABEL_FILE = '../data/labels.txt'
 
+app = flask.Flask(__name__)
+interpreter = None
+input_details = None
+output_details = None
+
+
+@app.route('/', methods=['GET'])
+def default():
+    image_path = '../data/trouble.jpg'
+    image = Image.open(image_path)
+    input_data = prepare_image(image)
+    classify(input_data)
+
+    return flask.jsonify({'success': True})
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    if flask.request.files.get('image'):
+        # read the image in PIL format
+        image = flask.request.files['image'].read()
+        image = Image.open(io.BytesIO(image))
+        input_data = prepare_image(image)
+        classify(input_data)
+
+        return flask.jsonify({'success': True})
+
+def load_model():
+    # Load TFLite model and allocate tensors.
+    global interpreter
+    interpreter = tflite.Interpreter(model_path=MODEL_FILE)
+    interpreter.allocate_tensors()
+
+    # Get input and output tensors.
+    global input_details, output_details
+    input_details = interpreter.get_input_details()
+    output_details = interpreter.get_output_details()
+
 
 def load_labels(filename):
     with open(filename, 'r') as f:
         return [line.strip() for line in f.readlines()]
 
 
-if __name__ == '__main__':
-    # Load TFLite model and allocate tensors.
-    interpreter = tflite.Interpreter(model_path=MODEL_FILE)
-    interpreter.allocate_tensors()
-
-    # Get input and output tensors.
-    input_details = interpreter.get_input_details()
-    output_details = interpreter.get_output_details()
-
-    # check the type of the input tensor
-    floating_model = input_details[0]['dtype'] == np.float32
-
+def prepare_image(pil_image):
     # Resize image
     height = input_details[0]['shape'][1]
     width = input_details[0]['shape'][2]
-    image_path = '../data/trouble.jpg'
-    img = Image.open(image_path).resize((width, height))
+    resized_image = pil_image.resize((width, height))
 
     # Add N dimension
-    input_data = np.expand_dims(img, axis=0)
+    return np.expand_dims(resized_image, axis=0)
 
+
+def classify(input_data):
     interpreter.set_tensor(input_details[0]['index'], input_data)
-
     interpreter.invoke()
-
-    # The function `get_tensor()` returns a copy of the tensor data.
-    # Use `tensor()` in order to get a pointer to the tensor.
     output_data = interpreter.get_tensor(output_details[0]['index'])
     results = np.squeeze(output_data)
     print(results)
@@ -52,3 +78,8 @@ if __name__ == '__main__':
     labels = load_labels(LABEL_FILE)
     for i in top_k:
         print('{:08.6f}: {}'.format(float(results[i] / 255.0), labels[i]))
+
+
+if __name__ == '__main__':
+    load_model()
+    app.run(host='0.0.0.0', port=5000)
