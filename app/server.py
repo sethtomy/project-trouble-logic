05680038cf707759c15bd1fa9f833c6a3b87421b
Seth <sethtomy@gmail.com>
2020-05-17 19:56:39 -0400

    removed heic support

--- app/server.py
+++ app/server.py
@@ -2,7 +2,6 @@ from PIL import Image
 import flask
 from flask_cors import CORS
 import io
-import pyheif
 
 from logic import classify, load_model
 
@@ -12,24 +11,14 @@ CORS(app)
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    try:
-        print(flask.request.files)
-        file = flask.request.files.get('image')
-        file_type = file.filename.split('.')[1].lower()
-        if file_type == 'heic':
-            print('Got HEIC image.')
-            heif_file = pyheif.read_heif(file)
-            image = Image.frombytes(mode=heif_file.mode, size=heif_file.size, data=heif_file.data)
-        else:
-            # read the image in PIL format
-            print('Got non-HEIC image.')
-            image = flask.request.files['image'].read()
-            image = Image.open(io.BytesIO(image))
+    print(flask.request.files)
+    if flask.request.files.get('image'):
+        print('Got image')
+        image = flask.request.files['image'].read()
+        image = Image.open(io.BytesIO(image))
         res = classify(image)
-        print('Successfully classified image.')
         return flask.jsonify(res)
-    except:
-        return 'Must pass image in form-data with label "image"', 415
+    return 'Must pass image in form-data with label "image"', 415
 
 
 if __name__ == '__main__':
