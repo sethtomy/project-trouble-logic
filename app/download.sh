e081a197ab28902816e4c9155c0968ef6bb957af
Seth <sethtomy@gmail.com>
2020-05-17 02:41:17 -0400

    Added libopenjp2-7 package

--- app/download.sh
+++ app/download.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+apt-get install libopenjp2-7
+
 python3 -m pip install -r requirements.txt
 
 pip3 install https://dl.google.com/coral/python/tflite_runtime-2.1.0.post1-cp37-cp37m-linux_armv7l.whl
